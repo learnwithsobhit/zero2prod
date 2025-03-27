62e2799b6476b532060584e23735e2c484b5c278
shobhit chaturvedi <learnwithshobhit@gmail.com>
2025-03-27 15:23:36 +0530

    updated tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,3 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-use actix_web::dev::Server;
-use std::net::TcpListener;
-use std::io;
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
-
-pub fn run(listener: TcpListener) -> Result<Server, io::Error> {
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-    })
-    .listen(listener)?
-    .run();
-    Ok(server)
-}
+pub mod configuration;
+pub mod routes;
+pub mod startup;
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 use std::net::TcpListener;
 
-use zero2prod::run;
+use sqlx::PgPool;
+use zero2prod::{configuration::get_configuration, startup::run};
+
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:8000")?;
-    run(listener)?.await
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+    let listener = TcpListener::bind(format!(
+        "{}:{}",
+        configuration.application_host, configuration.application_port
+    ))?;
+    run(listener, connection_pool)?.await
 }
