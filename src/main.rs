d7a1f83df0f4d9d05f095c6cc51c102c5d7426ed
shobhit chaturvedi <learnwithshobhit@gmail.com>
2025-03-29 15:57:34 +0530

    updated

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,23 @@
-use secrecy::ExposeSecret;
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
+use zero2prod::configuration::get_configuration;
+use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
-use zero2prod::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
-async fn main() -> Result<(), std::io::Error> {
+async fn main() -> std::io::Result<()> {
     let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool =
-        PgPool::connect(configuration.database.connection_string().expose_secret())
-            .await
-            .expect("Failed to connect to Postgres.");
-    let listener = TcpListener::bind(format!(
+        PgPoolOptions::new().connect_lazy_with(configuration.database.connect_options());
+
+    let address = format!(
         "{}:{}",
-        configuration.application_host, configuration.application_port
-    ))?;
-    run(listener, connection_pool)?.await
+        configuration.application.host, configuration.application.port
+    );
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection_pool)?.await?;
+    Ok(())
 }
